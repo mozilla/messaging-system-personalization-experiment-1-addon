00f9eb742237f94012bed19b89c1032a150a63d2
Fredrik Wollsén <fredrik@neam.se>
2019-12-18 10:09:25 +0200

    Formatting and linting

--- src/BernoulliNB.js
+++ src/BernoulliNB.js
@@ -1,3 +1,5 @@
+/* eslint no-unused-vars: ["error", { "varsIgnorePattern": "(BernoulliNB)" }]*/
+
 /**
  * Adapted from https://github.com/nok/sklearn-porter/blob/stable/examples/estimator/classifier/BernoulliNB/js/basics.pct.ipynb
  *
@@ -6,37 +8,35 @@
  * @param delProbs
  * @constructor
  */
-var BernoulliNB = function(priors, negProbs, delProbs) {
+const BernoulliNB = function(priors, negProbs, delProbs) {
+  this.priors = priors;
+  this.negProbs = negProbs;
+  this.delProbs = delProbs;
 
-    this.priors = priors;
-    this.negProbs = negProbs;
-    this.delProbs = delProbs;
+  this.predict = function(features) {
+    const nClasses = this.priors.length,
+      nFeatures = this.delProbs.length;
 
-    this.predict = function(features) {
-        var nClasses = this.priors.length,
-            nFeatures = this.delProbs.length;
+    const jll = new Array(nClasses);
+    for (let i = 0; i < nClasses; i++) {
+      let sum = 0;
+      for (let j = 0; j < nFeatures; j++) {
+        sum += features[i] * this.delProbs[j][i];
+      }
+      jll[i] = sum;
+    }
+    for (let i = 0; i < nClasses; i++) {
+      let sum = 0;
+      for (let j = 0; j < nFeatures; j++) {
+        sum += this.negProbs[i][j];
+      }
+      jll[i] += this.priors[i] + sum;
+    }
+    let classIdx = 0;
 
-        var jll = new Array(nClasses);
-        for (var i = 0; i < nClasses; i++) {
-            var sum = 0.;
-            for (var j = 0; j < nFeatures; j++) {
-                sum += features[i] * this.delProbs[j][i];
-            }
-            jll[i] = sum;
-        }
-        for (var i = 0; i < nClasses; i++) {
-            var sum = 0.;
-            for (var j = 0; j < nFeatures; j++) {
-                sum += this.negProbs[i][j];
-            }
-            jll[i] += this.priors[i] + sum;
-        }
-        var classIdx = 0;
-
-        for (var i = 0; i < nClasses; i++) {
-            classIdx = jll[i] > jll[classIdx] ? i : classIdx;
-        }
-        return classIdx;
-    };
-
+    for (let i = 0; i < nClasses; i++) {
+      classIdx = jll[i] > jll[classIdx] ? i : classIdx;
+    }
+    return classIdx;
+  };
 };
